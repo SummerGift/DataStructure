a9e094b1d04b8c2d790f9d0d31672b141e83a903
SummerGift <459994202@qq.com>
2017-10-20 22:18:51 +0800

    update templatefunc.cpp

--- cpp_learning/template_func/templatefunc.cpp
+++ cpp_learning/template_func/templatefunc.cpp
@@ -1,65 +1,34 @@
-// templatefunc.cpp: 定义控制台应用程序的入口点。
-//
-
 #include "stdafx.h"
-using namespace std;
 
-template <typename T>
-void swap(T& a,T& b)
+class value
 {
-	T t = a;
-	a = b;
-	b = t;
+private:
+	int mi;
 
-}
-
-template < typename T >
-class  Operator
-{
 public:
-	T process(T v)
+	value(int i)
 	{
-		return v * v;
+		printf("i = %d\n", i);
+		mi = i;	
 	}
 
-private:
+	int getmi()
+	{
+		return mi;
+	}
+
+	~value()
+	{
+		printf("program is over.\n");
+	}
 
 };
 
-namespace first_space {
-	void func() {
-		printf("Inside first_space\n");
-	}
-}
-
-namespace second_space {
-	void func() {
-		printf("Inside second_space\n");
-	}
-}
-
 int main()
 {
-	int a = 1;
-	int b = 2;
-	swap(a,b);
-	printf("a:%d,b:%d.\n",a,b);
+	value a(5);
 
-	double c = 0.01;
-	double d = 0.02;
-	swap(c,d);
-	printf("c:%lf,d:%lf\n",c,d);
-
-	Operator<int> opint;
-	printf(" 5 * 5 = %d.\n",opint.process(5));
-	Operator<double> opdouble;
-	printf(" 5 * 5 = %lf.\n", opdouble.process(5));
-
-	// 调用第一个命名空间中的函数
-	first_space::func();
-
-	// 调用第二个命名空间中的函数
-	second_space::func();
+	printf("the value a = %d.",a.getmi());
 
     return 0;
 }
