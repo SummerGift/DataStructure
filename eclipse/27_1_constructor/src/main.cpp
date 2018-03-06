2b61ba07cdd53a41ac53a99cb06ff67677bcd69d
SummerGift <459994202@qq.com>
2018-03-06 23:14:05 +0800

    完善数据结构代码。

--- eclipse/27_1_constructor/src/main.cpp
+++ eclipse/27_1_constructor/src/main.cpp
@@ -1,24 +1,56 @@
 #include <stdio.h>
-#include "IntArray.h"
+#include <time.h>
+#include <math.h>
 
-int main() {
-	IntArray* a = IntArray::NewInstance(5);
+clock_t start, stop;
+double duration;
+#define MAXN 10
+#define maxk 10000000
 
-	printf("a.length = %d\n", a->length());
+double f1( int n, double a[], double x )
+{
+	int i;
+	double p = a[0];
+	for ( i=1; i<=n; i++ )
+	p += (a[i] * pow(x, i));
+	return p;
+}
 
-	a->set(0, 1);
-	a->set(1, 1);
-	a->set(2, 1);
+double f2( int n, double a[], double x )
+{
+	int i;
+	double p = a[n];
+	for ( i=n; i>0; i-- )
+	p = a[i-1] + x*p;
+	return p;
+}
 
-	for (int i = 0; i < a->length(); i++) {
-		int v = 0;
-
-		a->get(i, v);
-
-		printf("a[%d] = %d\n", i, v);
+int main ()
+{
+	int i;
+	double a[MAXN];     /* 存储多项式的系数 */
+	for ( i=0; i<MAXN; i++ ) a[i] = (double)i;
+	start = clock();
+	for( i=0; i<maxk; i++)
+	{
+		f1(MAXN-1, a, 1.1);
 	}
 
-	delete a;
+	stop = clock();
+	duration = ((double)(stop - start))/CLK_TCK;
+	printf("ticks1 = %f\n", (double)(stop - start));
+	printf("duration1 = %6.2e\n", duration);
 
+	start = clock();
+
+	for( i=0; i<maxk; i++)
+	{
+		f2(MAXN-1, a, 1.1);
+	}
+
+	stop = clock();
+	duration = ((double)(stop - start))/CLK_TCK;
+	printf("ticks2 = %f\n", (double)(stop - start));
+	printf("duration2 = %6.2e\n", duration);
 	return 0;
 }
--- eclipse/32_string_class/src/String_Class.cpp
+++ eclipse/32_string_class/src/String_Class.cpp
@@ -1,43 +1,22 @@
-//============================================================================
-// Name        : String_Class.cpp
-// Author      : summergift
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-string reverse(const string& s, const char c)
-{
-    string ret = s;
-
-
-    return ret;
-}
-
 int main()
 {
-	cout << "hello" <<  endl;
-	string s1 = "hello";
-	string s2 = "mengbaobao";
-	cout << s1 + s2 <<  endl;
+    string s = "a1b2c3d4e345345";
+    int n = 0;
 
-	string test = "we;tonight;you";
-	cout << test << endl;
-	cout << test.length() << endl;
-	int pos = test.find(';');
-	cout << pos << endl;   //1,默认从位置0（即第1个字符）开始查找
-	string b = test.substr(0, pos);
-	cout << b << endl;
+    for(int i = 0; i<s.length(); i++)
+    {
+        if( isdigit(s[i]) )
+        {
+            n++;
+        }
+    }
 
-//    cout << reverse("", ';') << endl;                 // 输出：空字符串
-//    cout << reverse(";", ';') << endl;                // 输出：;
-//    cout << reverse("abcde;", ';') << endl;           // 输出：edcba;
-//    cout << reverse("we;tonight;you", ';') << endl;   // 输出：ew;thginot;uoy
+    cout << n << endl;
 
     return 0;
 }
