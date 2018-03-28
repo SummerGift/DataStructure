5fc8a6cf9e9493a001f6630bc6d1c9ce08840d58
SummerGift <459994202@qq.com>
2018-03-28 21:31:23 +0800

    [add]: update smart pointer code

--- eclipse/SummerGift_Lib/src/SmartPointer.h
+++ eclipse/SummerGift_Lib/src/SmartPointer.h
@@ -1,15 +1,52 @@
-/*
- * SmartPointer.h
- *
- *  Created on: 2018Äê3ÔÂ28ÈÕ
- *      Author: Summer
- */
-
 #ifndef SMARTPOINTER_H_
 #define SMARTPOINTER_H_
 
+#include <iostream>
+
+namespace SGLib
+{
 
 
+template <typename T>
+class SmartPointer
+{
 
+protected:
+	T* m_pointer;
+
+public:
+    SmartPointer(T* p = NULL)
+    {
+		m_pointer = p;
+    }
+
+	T* operator-> ()
+	{
+		return m_pointer;
+	}
+
+	T& operator* ()
+	{
+		return *m_pointer;
+	}
+
+	bool isNull()
+	{
+		return (m_pointer == NULL);
+	}
+
+	T* get()
+	{
+		return m_pointer;
+	}
+
+	~SmartPointer()
+	{
+		delete m_pointer;
+	}
+
+};
+
+}
 
 #endif /* SMARTPOINTER_H_ */
--- eclipse/SummerGift_Lib/src/SummerGift_Lib.cpp
+++ eclipse/SummerGift_Lib/src/SummerGift_Lib.cpp
@@ -1,13 +1,8 @@
-//============================================================================
-// Name        : SummerGift_Lib.cpp
-// Author      : summergift
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include "SmartPointer.h"
+
 using namespace std;
+using namespace SGLib;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
