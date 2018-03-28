674b33c85c0388489715023d0450620cbddf8181
SummerGift <459994202@qq.com>
2018-03-28 22:18:56 +0800

    [add]: SmartPointer code

--- eclipse/SummerGift_Lib/src/SmartPointer.h
+++ eclipse/SummerGift_Lib/src/SmartPointer.h
@@ -6,7 +6,6 @@
 namespace SGLib
 {
 
-
 template <typename T>
 class SmartPointer
 {
@@ -18,6 +17,29 @@ public:
     SmartPointer(T* p = NULL)
     {
 		m_pointer = p;
+    }
+
+    //拷贝构造函数，如果发生指针赋值，那么赋值之后让原来的指针指向NULL
+    SmartPointer(const SmartPointer<T>& obj)
+    {
+    	m_pointer = obj.m_pointer;
+    	//const_cast强制类型转换，去除一个对象的const属性
+    	const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
+    }
+
+   //重载 = 操作符
+    SmartPointer<T>& operator= (const SmartPointer<T>& obj)
+    {
+    	if (this != &obj)
+    	{
+    		//放掉 m_pointer 自身所指向的内存
+    		delete m_pointer;
+    		//指向所赋值的空间，然后让原指针指向NULL
+    		m_pointer = obj.m_pointer;
+    		const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
+    	}
+
+    	return *this;
     }
 
 	T* operator-> ()
