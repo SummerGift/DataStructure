68011438a88a1186dbe85861473a43c01b294403
SummerGift <459994202@qq.com>
2018-04-01 20:23:00 +0800

    【增加】：一元多项式的乘法和加法运算。

--- eclipse/DataStructuresCode/src/main.cpp
+++ eclipse/DataStructuresCode/src/main.cpp
@@ -1,56 +1,8 @@
-#include <iostream>
-
-using namespace std;
-
-template <typename T>
-void Swap(T &a,T &b)
-{
-	T t = a;
-	a = b;
-	b = t;
-}
-
-template <typename T>
-class Operator
-{
-public:
-	T op(T a,T b)
-	{
-		return a*b;
-	}
-};
-
-
-
-void demo1()
-{
-	throw 'c';
-}
+#include<stdio.h>
+#include<stdlib.h>
 
 
 int main() {
 
-	cout << "main begin" << endl;
-
-	try
-	{
-		demo1();
-	}
-	catch(int i)
-	{
-		cout << "catch(int i)" << endl;
-	}
-	catch(char i)
-	{
-		cout << "catch(char i)" << endl;
-	}
-	catch(...)
-	{
-		cout << "catch(...)" << endl;
-	}
-
-	cout << "main end" << endl;
-
 	return 0;
 }
-
