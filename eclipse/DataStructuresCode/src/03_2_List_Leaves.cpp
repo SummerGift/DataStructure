88026a3e729fb8f9f26c6edab7652b8e2de2e87b
SummerGift <459994202@qq.com>
2018-04-22 17:04:58 +0800

    【添加】：树的层序遍历，层序遍历树并且输出叶节点题目

--- eclipse/DataStructuresCode/src/03_2_List_Leaves.cpp
+++ eclipse/DataStructuresCode/src/03_2_List_Leaves.cpp
@@ -42,6 +42,9 @@ Sample Output:
 */
 
 
+//通过顺序输入，结构体数组存储的树，第一步是要找到这棵树的根节点
+//根节点有一个特殊的性质就是没有人指向它，所以需要一个标记数组来记录节点是否有其他节点指向它
+
 #include<iostream>
 #include<vector>
 #include<queue>
@@ -49,18 +52,102 @@ Sample Output:
 using namespace std;
 
 typedef int tree;
+#define Null -1
 
-int main()
-{
-	cout << "hello " << endl;
-	return 0;
+struct treenode {
+	int data;
+	tree left;
+	tree right;
+};
+
+//接收数据，将数据存储起来，建树，返回树的根节点
+tree buildtree(vector<treenode> &tree_in, int n) {
+	tree root = -1;
+	char cl, cr;
+	vector<int> check(n, 0);            //定义check包含 n 个 int 类型的元素，每个元素都被初始化为0
+
+	for (int i = 0; i < n; i++) {
+		cin >> cl >> cr;
+		tree_in[i].data = i;
+		if (cl != '-') {
+			tree_in[i].left = (int) (cl - '0');
+			check[tree_in[i].left] = 1;
+		} else {
+			tree_in[i].left = Null;
+		}
+
+		if (cr != '-') {
+			tree_in[i].right = (int) (cr - '0');
+			check[tree_in[i].right] = 1;
+		} else {
+			tree_in[i].right = Null;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+		if (!check[i]) {
+			root = i;
+			break;
+		}
+
+	return root;
 }
 
+/*
+queue 的基本操作有：
+入队，如例：q.push(x); 将x 接到队列的末端。
+出队，如例：q.pop(); 弹出队列的第一个元素，注意，并不会返回被弹出元素的值。
+访问队首元素，如例：q.front()，即最早被压入队列的元素。
+访问队尾元素，如例：q.back()，即最后被压入队列的元素。
+判断队列空，如例：q.empty()，当队列空时，返回true。
+访问队列中的元素个数，如例：q.size()
 
+push_back 容器类型在尾部加入数据。
+*/
 
+vector<int> findleaves(const vector<treenode> &tree_in, tree root) {
+	vector<int> leaves;
+	queue<treenode> save_quque;
+	treenode node;
 
+	if (root == Null)
+		return {};
+	save_quque.push(tree_in[root]);
+	while (!save_quque.empty()) {
+		node = save_quque.front();
+		save_quque.pop();
 
+		//如果该节点的左右儿子都不存在则为叶节点
+		if ((node.left == Null) && (node.right == Null))
+			leaves.push_back(node.data);
+		if (node.left != Null)
+			save_quque.push(tree_in[node.left]);
+		if (node.right != Null)
+			save_quque.push(tree_in[node.right]);
+	}
 
+	return leaves;
+}
 
+//decltype 查询表达式的数据类型
 
+/*
+int main() {
+	int n;
+	tree root;
 
+	cin >> n;
+	vector<treenode> Tree(10);
+	root = buildtree(Tree, n);
+
+	auto result = findleaves(Tree, root);   //接收返回的存储着叶子节点的队列，将队列遍历输出
+
+	for (decltype(result.size()) i = 0; i != result.size(); i++) {
+		cout << result[i];
+		//注意：最后一个数字输出后没有空格
+		i != result.size() - 1 ? cout << " " : cout << endl;
+	}
+
+	return 0;
+}
+*/
