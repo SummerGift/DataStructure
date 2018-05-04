f852de9db4bd657e003603200426b5bba3f9c45a
SummerGift <459994202@qq.com>
2018-05-04 22:05:11 +0800

    【完善】：修改代码风格，使用空格代替 TAB 建

--- Lectures/Project1-递归的思想与应用.md
+++ Lectures/Project1-递归的思想与应用.md
@@ -31,11 +31,11 @@
 
 ```c
 unsigned int sum(int n) {
-	if (n > 1)
-		return n + sum(n - 1);
-	else if (n == 1) {
-		return 1;
-	}
+    if (n > 1)
+        return n + sum(n - 1);
+    else if (n == 1) {
+        return 1;
+    }
 }
 ```
 
@@ -47,15 +47,15 @@ unsigned int sum(int n) {
 
 ```c
 unsigned int fac(int n) {
-	if (n >= 3) {
-		return fac(n - 1) + fac(n - 2);
-	}
+    if (n >= 3) {
+        return fac(n - 1) + fac(n - 2);
+    }
 
-	if (n == 2)
-		return 1;
+    if (n == 2)
+        return 1;
 
-	if (n == 1)
-		return 1;
+    if (n == 1)
+        return 1;
 }
 ```
 ### 2.3 用递归的方法编写函数求字符串长度
@@ -65,11 +65,11 @@ unsigned int fac(int n) {
 - 代码实现：
 
 ```c
-unsigned int get_strlen(char *s){
-	if(*s != '\0')
-		return 1 + get_strlen(s + 1);
-	if(*s == '\0')
-		return 0;
+unsigned int get_strlen(char *s) {
+    if (*s != '\0')
+        return 1 + get_strlen(s + 1);
+    if (*s == '\0')
+        return 0;
 }
 ```
 ### 2.4 单向链表的转置
@@ -80,15 +80,15 @@ unsigned int get_strlen(char *s){
 
 ```c
 List reverse(List list) {
-	if ((list == NULL || list->Next == NULL)) {
-		return list;
-	} else {
-		List guard = list->Next;
-		List ret = reverse(list->Next);
-		guard->Next = list;
-		list->Next = NULL;
-		return ret;
-	}
+    if ((list == NULL || list->Next == NULL)) {
+        return list;
+    } else {
+        List guard = list->Next;
+        List ret = reverse(list->Next);
+        guard->Next = list;
+        list->Next = NULL;
+        return ret;
+    }
 }
 ```
 
@@ -100,21 +100,21 @@ List reverse(List list) {
 
 ```c
 List List_Merge(List L1, List L2) {
-	if (L1 == NULL) {
-		return L2;
-	} else if (L2 == NULL) {
-		return L1;
-	} else if (L1->Data <= L2->Data) {
-		List list_1 = L1->Next;
-		List list = List_Merge(list_1, L2);
-		L1->Next = list;
-		return L1;
-	} else {
-		List list_2 = L2->Next;
-		List list = List_Merge(list_2, L1);
-		L2->Next = list;
-		return L2;
-	}
+    if (L1 == NULL) {
+        return L2;
+    } else if (L2 == NULL) {
+        return L1;
+    } else if (L1->Data <= L2->Data) {
+        List list_1 = L1->Next;
+        List list = List_Merge(list_1, L2);
+        L1->Next = list;
+        return L1;
+    } else {
+        List list_2 = L2->Next;
+        List list = List_Merge(list_2, L1);
+        L2->Next = list;
+        return L2;
+    }
 }
 ```
 
@@ -129,13 +129,13 @@ List List_Merge(List L1, List L2) {
 ```c
 //汉诺塔问题：从 a 到 c, b 为中转站
 void HanoiTower(int n, char a, char b, char c) {
-	if (n == 1) {
-		cout << a << "-->" << c << endl;
-	} else {
-		HanoiTower(n - 1, a, c, b);
-		HanoiTower(1, a, b, c);
-		HanoiTower(n - 1, b, a, c);
-	}
+    if (n == 1) {
+        cout << a << "-->" << c << endl;
+    } else {
+        HanoiTower(n - 1, a, c, b);
+        HanoiTower(1, a, b, c);
+        HanoiTower(n - 1, b, a, c);
+    }
 }
 ```
 
@@ -147,19 +147,19 @@ void HanoiTower(int n, char a, char b, char c) {
 
 ```c
 void permutation(char *s, char *e) {
-	if (*s == '\0') {
-		cout << e << endl;
-	} else {
-		int len = strlen(s);
+    if (*s == '\0') {
+        cout << e << endl;
+    } else {
+        int len = strlen(s);
 
-		for (int i = 0; i < len; i++) {
-			if ((i == 0) || (s[0] != s[i])) {
-				swap(s[0], s[i]);
-				permutation(s + 1, e);
-				swap(s[0], s[i]);
-			}
-		}
-	}
+        for (int i = 0; i < len; i++) {
+            if ((i == 0) || (s[0] != s[i])) {
+                swap(s[0], s[i]);
+                permutation(s + 1, e);
+                swap(s[0], s[i]);
+            }
+        }
+    }
 }
 ```
 
--- eclipse/DataStructuresCode/src/Project_01_recursive_function.cpp
+++ eclipse/DataStructuresCode/src/Project_01_recursive_function.cpp
@@ -7,7 +7,7 @@ typedef int ElementType;
 typedef struct Node *PtrToNode;
 struct Node {
     ElementType Data;
-    PtrToNode   Next;
+    PtrToNode Next;
 };
 typedef PtrToNode List;
 
@@ -15,96 +15,95 @@ using namespace std;
 
 //递归求和
 unsigned int sum(int n) {
-	if (n > 1)
-		return n + sum(n - 1);
-	else if (n == 1) {
-		return 1;
-	}
+    if (n > 1)
+        return n + sum(n - 1);
+    else if (n == 1) {
+        return 1;
+    }
 }
 
 //求斐波那契数列的第N个值
 unsigned int fac(int n) {
-	if (n >= 3) {
-		return fac(n - 1) + fac(n - 2);
-	}
+    if (n >= 3) {
+        return fac(n - 1) + fac(n - 2);
+    }
 
-	if (n == 2)
-		return 1;
+    if (n == 2)
+        return 1;
 
-	if (n == 1)
-		return 1;
+    if (n == 1)
+        return 1;
 }
 
 //求字符串的长度
-unsigned int get_strlen(char *s){
-	if(*s != '\0')
-		return 1 + get_strlen(s + 1);
-	if(*s == '\0')
-		return 0;
+unsigned int get_strlen(char *s) {
+    if (*s != '\0')
+        return 1 + get_strlen(s + 1);
+    if (*s == '\0')
+        return 0;
 }
 
 //链表的反转
 List reverse(List list) {
-	if ((list == NULL || list->Next == NULL)) {
-		return list;
-	} else {
-		List guard = list->Next;
-		List ret = reverse(list->Next);
-		guard->Next = list;
-		list->Next = NULL;
-		return ret;
-	}
+    if ((list == NULL || list->Next == NULL)) {
+        return list;
+    } else {
+        List guard = list->Next;
+        List ret = reverse(list->Next);
+        guard->Next = list;
+        list->Next = NULL;
+        return ret;
+    }
 }
 
 //有序链表的合并
 List List_Merge(List L1, List L2) {
-	if (L1 == NULL) {
-		return L2;
-	} else if (L2 == NULL) {
-		return L1;
-	} else if (L1->Data <= L2->Data) {
-		List list_1 = L1->Next;
-		List list = List_Merge(list_1, L2);
-		L1->Next = list;
-		return L1;
-	} else {
-		List list_2 = L2->Next;
-		List list = List_Merge(list_2, L1);
-		L2->Next = list;
-		return L2;
-	}
+    if (L1 == NULL) {
+        return L2;
+    } else if (L2 == NULL) {
+        return L1;
+    } else if (L1->Data <= L2->Data) {
+        List list_1 = L1->Next;
+        List list = List_Merge(list_1, L2);
+        L1->Next = list;
+        return L1;
+    } else {
+        List list_2 = L2->Next;
+        List list = List_Merge(list_2, L1);
+        L2->Next = list;
+        return L2;
+    }
 }
 
 //汉诺塔问题 从 a 到 c, b 为中转站
 void HanoiTower(int n, char a, char b, char c) {
-	if (n == 1) {
-		cout << a << "-->" << c << endl;
-	} else {
-		HanoiTower(n - 1, a, c, b);
-		HanoiTower(1, a, b, c);
-		HanoiTower(n - 1, b, a, c);
-	}
+    if (n == 1) {
+        cout << a << "-->" << c << endl;
+    } else {
+        HanoiTower(n - 1, a, c, b);
+        HanoiTower(1, a, b, c);
+        HanoiTower(n - 1, b, a, c);
+    }
 }
 
 //全排列问题
 void permutation(char *s, char *e) {
-	if (*s == '\0') {
-		cout << e << endl;
-	} else {
-		int len = strlen(s);
+    if (*s == '\0') {
+        cout << e << endl;
+    } else {
+        int len = strlen(s);
 
-		for (int i = 0; i < len; i++) {
-			if ((i == 0) || (s[0] != s[i])) {
-				swap(s[0], s[i]);
-				permutation(s + 1, e);
-				swap(s[0], s[i]);
-			}
-		}
-	}
+        for (int i = 0; i < len; i++) {
+            if ((i == 0) || (s[0] != s[i])) {
+                swap(s[0], s[i]);
+                permutation(s + 1, e);
+                swap(s[0], s[i]);
+            }
+        }
+    }
 }
 
-int main()
-{
+int main() {
 //	cout << "sum :" << sum(100) << endl;
 //
 //	for(int i = 1;i <= 10;i++)
@@ -117,5 +116,5 @@ int main()
 //	char string[] = "aaa";
 //	permutation(string,string);
 
-	return 0;
+    return 0;
 }
