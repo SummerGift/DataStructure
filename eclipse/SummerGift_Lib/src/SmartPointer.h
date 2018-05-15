e9068244b9a6ceb54c60df133664e5571e5f718e
SummerGift <459994202@qq.com>
2018-05-15 20:27:05 +0800

    【修改】：调整文档结构

--- eclipse/SummerGift_Lib/src/SmartPointer.h
+++ eclipse/SummerGift_Lib/src/SmartPointer.h
@@ -3,69 +3,54 @@
 
 #include <iostream>
 
-namespace SGLib
-{
+namespace SGLib {
 
-template <typename T>
-class SmartPointer
-{
+template<typename T>
+class SmartPointer {
 
 protected:
-	T* m_pointer;
+    T* m_pointer;
 
 public:
-    SmartPointer(T* p = NULL)
-    {
-		m_pointer = p;
+    SmartPointer(T* p = NULL) {
+        m_pointer = p;
     }
 
-    //拷贝构造函数，如果发生指针赋值，那么赋值之后让原来的指针指向NULL
-    SmartPointer(const SmartPointer<T>& obj)
-    {
-    	m_pointer = obj.m_pointer;
-    	//const_cast强制类型转换，去除一个对象的const属性
-    	const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
+    SmartPointer(const SmartPointer<T>& obj) {
+        m_pointer = obj.m_pointer;
+        const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
     }
 
-   //重载 = 操作符
-    SmartPointer<T>& operator= (const SmartPointer<T>& obj)
-    {
-    	if (this != &obj)
-    	{
-    		//放掉 m_pointer 自身所指向的内存
-    		delete m_pointer;
-    		//指向所赋值的空间，然后让原指针指向NULL
-    		m_pointer = obj.m_pointer;
-    		const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
-    	}
+    SmartPointer<T>& operator=(const SmartPointer<T>& obj) {
+        if (this != &obj) {
 
-    	return *this;
+            delete m_pointer;
+            m_pointer = obj.m_pointer;
+            const_cast<SmartPointer<T>&>(obj).m_pointer = NULL;
+        }
+
+        return *this;
     }
 
-	T* operator-> ()
-	{
-		return m_pointer;
-	}
+    T* operator->() {
+        return m_pointer;
+    }
 
-	T& operator* ()
-	{
-		return *m_pointer;
-	}
+    T& operator*() {
+        return *m_pointer;
+    }
 
-	bool isNull()
-	{
-		return (m_pointer == NULL);
-	}
+    bool isNull() {
+        return (m_pointer == NULL);
+    }
 
-	T* get()
-	{
-		return m_pointer;
-	}
+    T* get() {
+        return m_pointer;
+    }
 
-	~SmartPointer()
-	{
-		delete m_pointer;
-	}
+    ~SmartPointer() {
+        delete m_pointer;
+    }
 
 };
 
--- eclipse/SummerGift_Lib/src/SummerGift_Lib.cpp
+++ eclipse/SummerGift_Lib/src/SummerGift_Lib.cpp
@@ -4,32 +4,29 @@
 using namespace std;
 using namespace SGLib;
 
-class hellosummer
-{
+class hellosummer {
 public:
-	hellosummer()
-	{
-		cout<< "hello sumer" << endl;
-	}
+    hellosummer() {
+        cout << "hello sumer" << endl;
+    }
 
-	~hellosummer()
-	{
-		cout<< "goodbbye sumer" << endl;
-	}
+    ~hellosummer() {
+        cout << "goodbbye sumer" << endl;
+    }
 };
 
 int main() {
 
-	SmartPointer<hellosummer> sp = new hellosummer();
-	SmartPointer<hellosummer> s;
+    SmartPointer<hellosummer> sp = new hellosummer();
+    SmartPointer<hellosummer> s;
 
-	s = sp;
+    s = sp;
 
-	cout << "sp= " << sp.isNull() << endl;
-	cout << "s= " << s.isNull() << endl;
+    cout << "sp= " << sp.isNull() << endl;
+    cout << "s= " << s.isNull() << endl;
 
-	//s++;
+    //s++;
 
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+    cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+    return 0;
 }
