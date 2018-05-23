778c83bc5baa77fb4bde535aba7dca9362ddadf8
SummerGift <459994202@qq.com>
2018-05-23 22:04:43 +0800

    【添加】：file transfer 题目满分，实现按高度求并，union-by-height.

--- eclipse/DataStructuresCode/src/05_8_File_Transfer.cpp
+++ eclipse/DataStructuresCode/src/05_8_File_Transfer.cpp
@@ -74,45 +74,80 @@ The network is connected.
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MaxSize 100
-
 typedef int element_type;
-typedef int set_name;
-typedef element_type set_type[MaxSize];
+typedef int root_index;
+typedef int* root_type;
 
-set_name find(set_type S, element_type X) {
+//在本题中，关于集合的表示方法可以被简化，可以将N个元素映射到 0 - (N-1) 下标的数组中，
+//这样就不用像一般表示法那样遍历整个数组来寻找元素所在的位置
+//查找元素x所在位置的值，如果不是复数，那么就查找这个值对应位置的值，直到找到根结点所在的位置
+root_index find(root_type S, element_type X) {
     for (; S[X] >= 0; X = S[X])
         ;
     return X;
 }
 
-void union_root(set_type S, set_name root1, set_name root2) {
-    S[root2] = root1;
+void union_root(root_type S, root_index root1, root_index root2) {
+    S[root1] = root2;
 }
 
-void initialization(set_type S, int n) {
-
+void senior_union_root(root_type S, root_index root1, root_index root2) {
+    if (S[root2] < S[root1]) // root2 更深
+        S[root1] = root2;    // 将 root2 作为合并后的新根
+    else {
+        if (S[root2] == S[root1])
+            S[root1]--;      // 两棵树深度相同，选择一颗深度+1
+        S[root2] = root1;    // 合并
+    }
 }
 
-void input_connection(set_type S) {
-    element_type u,v;
-    set_name root1,root2;
-    scanf("%d %d\n", &u,&v);
+void initialization(root_type S, int n) {
+    for (int i = 0; i < n; i++)
+        S[i] = -1;
+}
+
+void input_connection(root_type S) {
+    element_type u, v;
+    root_index root1, root2;
+    scanf("%d %d\n", &u, &v);
     root1 = find(S, u - 1);
-    root1 = find(S, v - 1);
+    root2 = find(S, v - 1);
+    if (root1 != root2)
+        senior_union_root(S, root1, root2);
 }
 
-void check_connection(set_type S) {
+void check_connection(root_type S) {
+    element_type u, v;
+    root_index root1, root2;
+    scanf("%d %d\n", &u, &v);
+    root1 = find(S, u - 1);    //这里-1 是因为第u个元素在数组中的位置为u - 1
+    root2 = find(S, v - 1);
+    if (root1 == root2)
+        printf("yes\n");
+    else
+        printf("no\n");
 }
 
-void check_network(set_type S, int n) {
+void check_network(root_type S, int n) {
+    int i, counter = 0;
+    for (i = 0; i < n; i++) {
+        if (S[i] < 0)
+            counter++;
+    }
+
+    if (counter == 1)
+        printf("The network is connected.\n");
+    else
+        printf("There are %d components.\n", counter);
 }
 
 int main() {
-    set_type S;
+    int *S;
     int n;
     char input_char;
     scanf("%d\n", &n);
+    S = (int*) malloc(sizeof(int) * n);
+    initialization(S, n);
     do {
         scanf("%c", &input_char);
         switch (input_char) {
@@ -127,5 +162,6 @@ int main() {
             break;
         }
     } while (input_char != 'S');
+    free(S);
     return 0;
 }
