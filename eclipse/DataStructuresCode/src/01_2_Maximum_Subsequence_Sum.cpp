70878451e47af33090e456d157b5ed78f54a03ce
SummerGift <summergift2019@gmail.com>
2022-07-02 10:25:18 +0800

    【更新】maximum subsequence sum

--- eclipse/DataStructuresCode/src/01_2_Maximum_Subsequence_Sum.cpp
+++ eclipse/DataStructuresCode/src/01_2_Maximum_Subsequence_Sum.cpp
@@ -31,12 +31,12 @@ Sample Output:
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
-static int MaxSubseqSum2( int A[], int N )
-{   int ThisSum, MaxSum;
-    int i,array_end;
+static int MaxSubseqSum2(int A[], int N)
+{
+    int ThisSum, MaxSum;
+    int i, array_end;
     ThisSum = MaxSum = 0;
     array_end = 0;
     int array_begin = 0;
@@ -44,57 +44,57 @@ static int MaxSubseqSum2( int A[], int N )
     int zero_count = 1;
     int save_array_begin = 0;
 
-    for( i = 0; i < N; i++ )
+    for (i = 0; i < N; i++)
     {
         ThisSum += A[i]; /* 向右累加 */
 
-        if( A[i] >= 0 )
+        if (A[i] >= 0)
             allnative = 0;
 
-        if( ThisSum > MaxSum )
+        if (ThisSum > MaxSum)
         {
             MaxSum = ThisSum; /* 发现更大和则更新当前结果 */
             zero_count++;
             array_end = i;
             save_array_begin = array_begin;
-        }else
-        if( ThisSum < 0 ) /* 如果当前子列和为负 */
+        }
+        else if (ThisSum < 0) /* 如果当前子列和为负 */
         {
-            ThisSum = 0 ; /* 则不可能使后面的部分和增大，抛弃之 */
+            ThisSum = 0; /* 则不可能使后面的部分和增大，抛弃之 */
             array_begin = i + 1;
         }
     }
 
-    if ( allnative )
+    if (allnative)
     {
-        printf("0 %d %d",A[0], A[ N - 1 ]);
+        printf("0 %d %d", A[0], A[N - 1]);
         return 0;
     }
 
-    if( MaxSum == 0 )
+    if (MaxSum == 0)
     {
         printf("0 0 0");
-    }else
+    }
+    else
     {
-        printf("%d %d %d",MaxSum,A[save_array_begin],A[array_end]);
+        printf("%d %d %d", MaxSum, A[save_array_begin], A[array_end]);
     }
 
     return 0;
 }
 
-
-//int main()
+// int main()
 //{
-//    int i, *a;
-//    int arraycount;
-//    scanf("%d",&arraycount);
+//     int i, *a;
+//     int arraycount;
+//     scanf("%d",&arraycount);
 //
-//    a = (int *)malloc(sizeof(int) * arraycount); /*分配内存*/
+//     a = (int *)malloc(sizeof(int) * arraycount); /*分配内存*/
 //
-//    for(i = 0; i < arraycount; i++)
-//        scanf("%d", a + i);
+//     for(i = 0; i < arraycount; i++)
+//         scanf("%d", a + i);
 //
-//    MaxSubseqSum2(a, arraycount);
-//    free(a);
-//    return 0;
-//}
+//     MaxSubseqSum2(a, arraycount);
+//     free(a);
+//     return 0;
+// }
