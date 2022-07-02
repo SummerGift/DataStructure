1103b05bc34be926fa734f05415bd52cfa5f733c
SummerGift <summergift2019@gmail.com>
2022-07-02 10:28:10 +0800

    [update] max subsquencesum q1

--- eclipse/DataStructuresCode/src/01_1_Maxsubsequencesum_question.cpp
+++ eclipse/DataStructuresCode/src/01_1_Maxsubsequencesum_question.cpp
@@ -40,37 +40,36 @@
 
 #include <stdio.h>
 
-static int MaxSubseqSum1( int A[], int N )
-{   int ThisSum, MaxSum;
-    int i;
-    ThisSum = MaxSum = 0;
-    for( i = 0; i < N; i++ ) {
-          ThisSum += A[i]; /* 向右累加 */
-          if( ThisSum > MaxSum )
-                  MaxSum = ThisSum; /* 发现更大和则更新当前结果 */
-          else if( ThisSum < 0 ) /* 如果当前子列和为负 */
-                  ThisSum = 0; /* 则不可能使后面的部分和增大，抛弃之 */
-    }
-    return MaxSum;
+static int MaxSubseqSum1(int A[], int N)
+{
+        int ThisSum, MaxSum;
+        int i;
+        ThisSum = MaxSum = 0;
+        for (i = 0; i < N; i++)
+        {
+                ThisSum += A[i]; /* 向右累加 */
+                if (ThisSum > MaxSum)
+                        MaxSum = ThisSum; /* 发现更大和则更新当前结果 */
+                else if (ThisSum < 0)     /* 如果当前子列和为负 */
+                        ThisSum = 0;      /* 则不可能使后面的部分和增大，抛弃之 */
+        }
+        return MaxSum;
 }
 
-//int main()
+// int main()
 //{
-//    int i, *a;
-//    int arraycount;
-//    int result;
-//    scanf("%d",&arraycount);
+//     int i, *a;
+//     int arraycount;
+//     int result;
+//     scanf("%d",&arraycount);
 //
-//    a = malloc(sizeof(int) * arraycount); /*分配内存*/
+//     a = malloc(sizeof(int) * arraycount); /*分配内存*/
 //
-//    for(i = 0; i < arraycount; i++)
-//        scanf("%d", a + i);
+//     for(i = 0; i < arraycount; i++)
+//         scanf("%d", a + i);
 //
-//    result = MaxSubseqSum1(a, arraycount);
-//    printf("%d",result);
+//     result = MaxSubseqSum1(a, arraycount);
+//     printf("%d",result);
 //
-//  return 0;
-//}
-
-
-
+//   return 0;
+// }
